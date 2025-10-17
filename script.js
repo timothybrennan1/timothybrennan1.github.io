832e7f5f5e3468966e66f6e93ee5a8acce43ec5e
TimBrennan <timbrennanstl@gmail.com>
2025-10-17 14:03:26 -0500

    Adding hamburger nav bar

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+
+// Mobile menu toggle
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+
+mobileMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  mobileMenu.classList.toggle("open");
+});
+

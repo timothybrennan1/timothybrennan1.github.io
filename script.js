f41a9001e3b0e05493a499216ed12efaeb61786c
TimBrennan <timbrennanstl@gmail.com>
2025-10-17 15:36:52 -0500

    Nav Hamburger Menu Change

--- script.js
+++ script.js
@@ -12,9 +12,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Mobile menu toggle
 const mobileMenu = document.getElementById("mobile-menu");
 const navLinks = document.querySelector(".nav-links");
+// Select all links in the mobile menu
+const navLinkItems = document.querySelectorAll(".nav-links li a");
+
+navLinkItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active"); // close menu
+  });
+});
 
 mobileMenu.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-  mobileMenu.classList.toggle("open");
 });
 
